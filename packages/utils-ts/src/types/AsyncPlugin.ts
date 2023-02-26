9660464308bcdd851a9aaebf31e85fdf13fd5092
Sjoerd <sjoerddal108@gmail.com>
2023-02-26 21:44:19 +0100

    Simplified async plugin type

--- packages/utils-ts/src/types/AsyncPlugin.ts
+++ packages/utils-ts/src/types/AsyncPlugin.ts
@@ -1,39 +1,22 @@
 import { ImageCallback } from "@jimp/core";
 
-type Plugin<Base> = {
-  [K in keyof Base]: (...args: any[]) => any;
-};
-
-type MethodNames<PluginInterface> = keyof PluginInterface;
-
-type AsyncMethodNames<PluginInterface> = {
-  [K in MethodNames<PluginInterface>]: `${K extends Extract<
-    keyof PluginInterface,
-    string
-  >
-    ? K
-    : never}Async`;
-};
-
-type GenerateAsyncMethods<PluginInterface> = {
-  [K in AsyncMethodNames<PluginInterface>[MethodNames<PluginInterface>]]: PluginInterface[MethodNames<PluginInterface>];
-};
-
 type PromisifiedFunction<T> = T extends (
+  this: infer This,
   ...args: [...infer Args, infer Callback]
-) => any
-  ? Callback extends ImageCallback<unknown>
+) => unknown
+  ? Callback extends ImageCallback<This>
     ? (...args: Args) => Promise<Parameters<Callback>[1]>
     : (...args: Args) => Promise<any>
   : never;
 
-type PromisifiedInterface<Interface extends Plugin<Interface>> = {
-  [K in keyof Interface]: PromisifiedFunction<Interface[keyof Interface]>;
+type Asyncify<BasePlugin> = {
+  [K in keyof BasePlugin as K extends string
+    ? `${K}Async`
+    : never]: PromisifiedFunction<BasePlugin[K]>;
 };
 
 /**
  * Type that enforces that a plugin not only implements the upstream interface correctly but also has an async variant
  * of the method available.
  */
-export type AsyncPlugin<BasePlugin extends Plugin<BasePlugin>> =
-  PromisifiedInterface<GenerateAsyncMethods<BasePlugin>>;
+export type AsyncPlugin<BasePlugin> = BasePlugin & Asyncify<BasePlugin>;
