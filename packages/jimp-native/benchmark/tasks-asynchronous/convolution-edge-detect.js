9c7be05e50679c32f4ef3da50991db27efb8a61a
Sjoerd <sjoerddal108@gmail.com>
2023-04-14 00:34:27 +0200

    Fix benchmarks

--- packages/jimp-native/benchmark/tasks-asynchronous/convolution-edge-detect.js
+++ packages/jimp-native/benchmark/tasks-asynchronous/convolution-edge-detect.js
@@ -1,13 +1,11 @@
-const GAUSSIAN_KERNEL = [
-  [1 / 256, 4 / 256, 6 / 256, 4 / 256, 1 / 256],
-  [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
-  [6 / 256, 24 / 256, 36 / 256, 24 / 256, 6 / 256],
-  [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
-  [1 / 256, 4 / 256, 6 / 256, 4 / 256, 1 / 256],
+const EDGE_DETECT_KERNEL = [
+  [-1, -1, -1],
+  [-1, 8, -1],
+  [-1, -1, -1],
 ];
 
 module.exports = (image) => {
   return new Promise((resolve) => {
-    image.convolution(GAUSSIAN_KERNEL, resolve);
+    image.convolution(EDGE_DETECT_KERNEL, resolve);
   });
 };
--- packages/jimp-native/benchmark/tasks-asynchronous/convolution-gaussian-blur.js
+++ packages/jimp-native/benchmark/tasks-asynchronous/convolution-gaussian-blur.js
@@ -1,11 +1,13 @@
-const EDGE_DETECT_KERNEL = [
-  [-1, -1, -1],
-  [-1, 8, -1],
-  [-1, -1, -1],
+const GAUSSIAN_KERNEL = [
+  [1 / 256, 4 / 256, 6 / 256, 4 / 256, 1 / 256],
+  [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
+  [6 / 256, 24 / 256, 36 / 256, 24 / 256, 6 / 256],
+  [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
+  [1 / 256, 4 / 256, 6 / 256, 4 / 256, 1 / 256],
 ];
 
 module.exports = (image) => {
   return new Promise((resolve) => {
-    image.convolution(EDGE_DETECT_KERNEL, resolve);
+    image.convolution(GAUSSIAN_KERNEL, resolve);
   });
 };
