6ddbcc76738da09861daccb4df625832cf570683
Sjoerd <sjoerddal108@gmail.com>
2023-04-15 14:24:09 +0200

    Don't rely on UNIX-style output redirection for binding.gypi

--- packages/utils-napi/printBinding.js
+++ packages/utils-napi/printBinding.js
@@ -1,4 +1,8 @@
+/**
+ * Script that creates a binding.gypi file in the project it's required from.
+ */
 const fs = require("fs");
 const path = require("path");
 
-console.log(fs.readFileSync(path.join(__dirname, "binding.gypi"), "utf8"));
+const binding = fs.readFileSync(path.join(__dirname, "binding.gypi"), "utf8");
+fs.writeFileSync(path.join(process.cwd(), "binding.gypi"), binding);
